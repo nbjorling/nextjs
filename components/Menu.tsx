0cafd0e99234109a0099032a114d05908402f978
Niklas Björling <niklas.cj.bjorling@gmail.com>
2022-11-01 10:34:17 +0100

    ICONS

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -5,7 +5,7 @@ const LinkElement = ({ href, title }) => {
   return (
     <Link href={href} passHref>
       <a className="text-xl cursor-pointer mb-2 pl-0 hover:pl-1 transition-all">
-        <div className="hover:text-cyan-200 select-none">{title}</div>
+        <div className="hover:text-cyan-200 mb-2 select-none">{title}</div>
       </a>
     </Link>
   );
@@ -22,7 +22,7 @@ export const Menu = () => {
     <div className={`app-menu font-convergence  ${isMenuOpen ? "menu-open" : ""}`}>
       <div className="" onClick={() => toggleMenu()}>
         <div className="flex bg-slate-900 text-white border-slate-900 shadow-lg border-b pl-8 p-2 cursor-pointer select-none">
-          {isMenuOpen ? "Close Menu" : "Menu"}
+          {isMenuOpen ? "🍔 Close Menu" : "🍔 Menu"}
         </div>
       </div>
       <div
@@ -32,8 +32,8 @@ export const Menu = () => {
       >
         <div className={"bg-slate-900 border-t-slate-600 border-t h-full  text-white flex-row relative"}>
           <div className="p-10">
-            <LinkElement href="/" title="Start page" />
-            <LinkElement href="/yahtzee" title="Yahtzee" />
+            <LinkElement href="/" title="🏠 Start page" />
+            <LinkElement href="/yahtzee" title="🎲 Yahtzee" />
           </div>
           <div className="absolute bottom-8 w-full h-12 bg-slate-900 flex shadow-lg">
             <div className="text-center items-center w-full p-2">Made By Björling</div>
--- pages/yahtzee.tsx
+++ pages/yahtzee.tsx
@@ -1,11 +1,10 @@
 import Link from "next/link";
 import * as React from "react";
 import { Yahtzee } from "../components/Yahtzee/Yahtzee";
-import { Menu } from "../components/Menu";
 
 export default function StartPage() {
   return (
-    <main className="w-full min-h-screen bg-slate-800">
+    <main className="w-full min-h-screen bg-slate-800 pt-4">
       <Link href="/">
         <div className="text-white ml-4 mr-4 cursor-pointer bg-slate-700 rounded py-2 px-4 w-fit">Back</div>
       </Link>
