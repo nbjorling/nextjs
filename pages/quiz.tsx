15c6c2e622d1fe3465d7ce2201060b0e7e4bb20b
Niklas Björling <31563031+nbjorling@users.noreply.github.com>
2022-11-30 16:47:35 +0100

    update quiz component with some styling and functionality

--- pages/quiz.tsx
+++ pages/quiz.tsx
@@ -53,12 +53,52 @@ const QUIZ_DATA: QuizDataProps = {
       ],
       correctAnswer: 2,
     },
+    {
+      question:
+        "Saying the name of what dried fruit used to be used to encourage people to smile before a photo in the 1800s, before the phrase “cheese?”",
+      choices: [
+        { text: "Prunes", correct: false },
+        { text: "Kiwis", correct: false },
+        {
+          text: "En låååååååång text för att se hur ett långt svar skulle kunna se ut i denna quiz",
+          correct: true,
+        },
+      ],
+      correctAnswer: 2,
+    },
   ],
 };
 
-const Choice: React.FC<ChoiceElementProps> = ({ choice, selected, onClick }) => {
-  const bgStyle = selected && "border-yellow-400";
-  const styles = "border-2 p-2 my-4 cursor-pointer bg-slate-500  hover:bg-slate-400";
+const AnimatedBorderContainer: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  return (
+    <div className="relative overflow-hidden w-full min-h-[200px] h-[70vh] shadow-xl rounded-lg">
+      <div
+        className="absolute z-0 -left-[100%] -top-[100%] w-[300%] h-[300%] animate-rotate"
+        style={{
+          background:
+            "linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab)",
+          backgroundSize: "100% 100%",
+          transform: "translateY(0)",
+          transformOrigin: "0, 0",
+        }}
+      ></div>
+      <div className="absolute z-10 left-[2px] top-[2px] w-[calc(100%-4px)] h-[calc(100%-4px)] rounded-lg bg-slate-700 p-4 transition-color duration-300 ">
+        {children}
+      </div>
+    </div>
+  );
+};
+
+const Choice: React.FC<ChoiceElementProps> = ({
+  choice,
+  selected,
+  onClick,
+}) => {
+  const bgStyle = selected && "outline-2 outline-slate-300";
+  const styles =
+    "p-2 my-4 cursor-pointer bg-slate-600  hover:bg-slate-500 rounded";
   return (
     <div className={[bgStyle, styles].join(" ")} onClick={onClick}>
       <div className="flex justify-between">
@@ -69,18 +109,28 @@ const Choice: React.FC<ChoiceElementProps> = ({ choice, selected, onClick }) => 
   );
 };
 
-const Question: React.FC<QuestionComponentProps> = ({ question, onClick, questionChoice, questionIndex }) => {
+const Question: React.FC<QuestionComponentProps> = ({
+  question,
+  onClick,
+  questionChoice,
+  questionIndex,
+}) => {
   return (
-    <div className="text-white bg-slate-600 m-4 p-4 rounded-lg">
-      <div>
-        {questionIndex + 1}. {question?.question}
-      </div>
+    <div className="text-white p-4">
+      <div>{question?.question}</div>
       {question?.choices?.map((choice, index) => {
         const clickHandler = () => {
           onClick({ questionIndex, choiceIndex: index });
         };
 
-        return <Choice onClick={clickHandler} key={index} choice={choice} selected={questionChoice === index} />;
+        return (
+          <Choice
+            onClick={clickHandler}
+            key={index}
+            choice={choice}
+            selected={questionChoice === index}
+          />
+        );
       })}
     </div>
   );
@@ -91,7 +141,7 @@ export default function Quiz() {
   const [choices, setChoices] = React.useState<number[]>([]);
   const [showAnswers, setShowAnswers] = React.useState<boolean>(false);
   const [currentQuestion, setCurrentQuestion] = React.useState<number>(0);
-
+  const nQuestions = QUIZ_DATA?.questions.length;
   const handleChoiceClick = ({ questionIndex, choiceIndex }) => {
     const newChoices = [...choices];
     newChoices[questionIndex] = choiceIndex;
@@ -110,35 +160,60 @@ export default function Quiz() {
 
   return (
     <main className="w-full min-h-screen bg-slate-800 pt-4 font-hyperlegible">
-      <div className="fixed bottom-0 p-4 w-40 h-auto bg-white ">
+      {/* <div className="fixed bottom-0 p-4 w-40 h-auto bg-white ">
         JSON DATA: <br></br>
         {JSON.stringify(choices, null, 2)}
+      </div> */}
+      <div className="p-4">
+        <h2 className="text-white mb-4">
+          Question {currentQuestion + 1} of {nQuestions}
+        </h2>
+        <AnimatedBorderContainer>
+          <Question
+            onClick={handleChoiceClick}
+            questionIndex={currentQuestion}
+            key={currentQuestion}
+            questionChoice={choices[currentQuestion]}
+            question={QUIZ_DATA.questions[currentQuestion]}
+          />
+        </AnimatedBorderContainer>
+        <div className="flex w-full justify-center">
+          <div className="flex-grow">
+            {currentQuestion !== 0 && (
+              <button
+                className="bg-slate-200 py-2 px-4 mt-4 mr-2 rounded"
+                onClick={() => setCurrentQuestion((prev) => prev - 1)}
+              >
+                Previous
+              </button>
+            )}
+
+            {currentQuestion + 1 < nQuestions && (
+              <button
+                className="bg-slate-200 py-2 px-4 mr-2 mt-4 rounded"
+                onClick={() => setCurrentQuestion((prev) => prev + 1)}
+              >
+                Next
+              </button>
+            )}
+          </div>
+
+          <button
+            className="bg-slate-200 py-2 px-4 mr-2 mt-4 rounded"
+            onClick={() => submit()}
+          >
+            Rätta
+          </button>
+        </div>
       </div>
-      <Question
-        onClick={handleChoiceClick}
-        questionIndex={currentQuestion}
-        key={currentQuestion}
-        questionChoice={choices[currentQuestion]}
-        question={QUIZ_DATA.questions[currentQuestion]}
-      />
-      <div className="flex w-full justify-center">
-        <button
-          className="bg-slate-200 py-2 px-4 mt-4 mr-2 rounded"
-          onClick={() => setCurrentQuestion((prev) => prev - 1)}
+      {showAnswers && (
+        <div
+          className="absolute text-center z-20 w-full top-1/2 text-2xl bg-yellow-400 curspor-pointer"
+          onClick={() => setShowAnswers(false)}
         >
-          Previous
-        </button>
-        <button className="bg-slate-200 py-2 px-4 mr-2 mt-4 rounded" onClick={() => submit()}>
-          Rätta
-        </button>
-        <button
-          className="bg-slate-200 py-2 px-4 mr-2 mt-4 rounded"
-          onClick={() => setCurrentQuestion((prev) => prev + 1)}
-        >
-          Next
-        </button>
-      </div>
-      {showAnswers && <div className="text-2xl p-2 m-8 bg-yellow-400">{points.toString()} Correct Answers</div>}
+          {points.toString()} Correct Answers
+        </div>
+      )}
     </main>
   );
 }
