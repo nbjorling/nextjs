4ff8edcec1c4913b7156b95884c7c29dcd708d99
Jack Johnsson <jack@globalmouth.com>
2023-02-17 12:10:48 +0100

    add keys to iterators

--- components/JeopardyCard/JeopardyCard.tsx
+++ components/JeopardyCard/JeopardyCard.tsx
@@ -1,6 +1,6 @@
-import * as React from "react";
+import * as React from 'react';
 
-type JeopardyCardProps = { caption: string, question?: string };
+type JeopardyCardProps = { caption: string; question?: string };
 
 const JeopardyCard: React.FC<JeopardyCardProps> = ({ caption, question }) => {
   const [showQuestion, setShowQuestion] = React.useState(false);
@@ -11,22 +11,22 @@ const JeopardyCard: React.FC<JeopardyCardProps> = ({ caption, question }) => {
 
   return (
     <div
-      className="flex m-1 h-32 w-48 bg-transparent [perspective:1000px]"
+      className='m-1 flex h-32 w-48 cursor-pointer select-none bg-transparent [perspective:1000px]'
       onClick={flipCard}
     >
       <div
-        className={`relative transition-all w-full h-full duration-500 [transform-style:preserve-3d] ${
-          showQuestion && "[transform:rotateY(180deg)]"
+        className={`relative h-full w-full transition-all duration-500 [transform-style:preserve-3d] ${
+          showQuestion && '[transform:rotateY(180deg)]'
         }`}
       >
-        <div className="absolute inset-0 [backface-visibility:hidden]">
-          <div className="bg-blue-500 h-full w-full flex justify-center items-center flex-col">
-            <p className="text-yellow-300 text-4xl text-center">{caption}</p>
+        <div className='absolute inset-0 [backface-visibility:hidden]'>
+          <div className='flex h-full w-full flex-col items-center justify-center bg-blue-500'>
+            <p className='text-center text-4xl text-yellow-300'>{caption}</p>
           </div>
         </div>
-        <div className="absolute inset-0 h-full w-full [transform:rotateY(180deg)] [backface-visibility:hidden]">
-          <div className="bg-blue-500 h-full w-full flex justify-center items-center flex-col">
-            <p className="text-yellow-300 text-xl text-center">{question}</p>
+        <div className='absolute inset-0 h-full w-full [transform:rotateY(180deg)] [backface-visibility:hidden]'>
+          <div className='flex h-full w-full flex-col items-center justify-center bg-blue-500'>
+            <p className='text-center text-xl text-yellow-300'>{question}</p>
           </div>
         </div>
       </div>
--- pages/jeopardy.tsx
+++ pages/jeopardy.tsx
@@ -1,16 +1,22 @@
-import * as React from "react";
-import JeopardyCard from "../components/JeopardyCard/JeopardyCard";
-import categories from "../public/jeopardy.json";
+import * as React from 'react';
+import JeopardyCard from '../components/JeopardyCard/JeopardyCard';
+import categories from '../public/jeopardy.json';
 
 const Jeopardy: React.FC = () => {
   return (
-    <div className="bg-black flex flex-row h-full w-full">
+    <div className='flex h-full w-full flex-row bg-black'>
       {categories.map((category) => (
-        <div className="flex-col">
+        <div key={category.name} className='flex-col'>
           <JeopardyCard caption={category.name} />
           {category.questions.map((question, index) => {
             const caption = `${(index + 1) * 100}`;
-            return <JeopardyCard caption={caption} question={question} />;
+            return (
+              <JeopardyCard
+                key={question}
+                caption={caption}
+                question={question}
+              />
+            );
           })}
         </div>
       ))}
