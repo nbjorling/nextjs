2e90f0f1f64d7947bc8e6ddd8f5258f5acdd4141
Niklas Björling <31563031+nbjorling@users.noreply.github.com>
2023-04-06 11:52:01 +0200

    BUMP Next Version

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'upload.wikimedia.org',
+        hostname: '**',
+        port: '',
+        // pathname: '/account123/**',
+      },
+    ],
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
