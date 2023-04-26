c974ea018d396d54abc6efb59500cc48a595a6ab
Niklas Björling <31563031+nbjorling@users.noreply.github.com>
2023-04-26 11:52:36 +0200

    Update concerts.tsx

--- pages/concerts.tsx
+++ pages/concerts.tsx
@@ -60,9 +60,6 @@ const concerts = [
     city: 'Stockholm',
     img: 'https://upload.wikimedia.org/wikipedia/commons/8/8e/Panetoz_p%C3%A5_presentationen_Mello_2023_Link%C3%B6ping_1.jpg',
   },
-];
-
-const upcomingConcerts = [
   {
     name: 'Roger Waters',
     date: '2023-04-15',
@@ -71,7 +68,35 @@ const upcomingConcerts = [
     city: 'Stockholm',
     img: 'https://upload.wikimedia.org/wikipedia/commons/5/50/Roger_Waters_Newport_Folk_Festival_2015.jpg',
   },
+  {
+    name: 'Post Malone',
+    date: '2023-04-26',
+    time: '19:30',
+    place: 'Avicii Arena',
+    city: 'Stockholm',
+    img: 'https://upload.wikimedia.org/wikipedia/commons/6/63/CHRISALLMEID_CHRISALLMEID_POSTMALONE-3.jpg',
+  },
 ];
+
+const upcomingConcerts = [
+  {
+    name: 'Kygo',
+    date: '2023-06-31',
+    time: '??',
+    place: 'Lollapalooza',
+    city: 'Stockholm',
+    img: 'https://upload.wikimedia.org/wikipedia/commons/d/dd/Kygo_%2828481718120%29_%282%29.jpg',
+  },
+  {
+    name: 'Mumford and Sons',
+    date: '2023-07-01',
+    time: '??',
+    place: 'Lollapalooza',
+    city: 'Stockholm',
+    img: 'https://upload.wikimedia.org/wikipedia/commons/2/20/MS2015.jpg',
+  },
+];
+
 const ArtistCard = ({ concertData }: { concertData: ConcertProps }) => {
   return (
     <figure className='relative mb-4 aspect-video w-full cursor-pointer overflow-hidden rounded-lg bg-black shadow-md filter transition-all duration-300 hover:grayscale'>
