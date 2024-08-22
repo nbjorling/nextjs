46bceaa1afcf81f33218f986fa9609b7060c63b4
Niklas Björling <niklas.bjorling@deltaceros.com>
2024-08-22 11:58:51 +0300

    Remove diceroller from scorecard

--- pages/yahtzee.tsx
+++ pages/yahtzee.tsx
@@ -1,6 +1,5 @@
 import Link from 'next/link';
 import { useState } from 'react';
-import DiceRoller from '../components/DiceRoller/DiceRoller';
 import { Yahtzee } from '../components/Yahtzee/Yahtzee';
 
 export default function StartPage() {
@@ -13,15 +12,7 @@ export default function StartPage() {
             Back
           </div>
         </Link>
-        <button
-          type='button'
-          className='ml-4 mr-4 w-fit cursor-pointer rounded bg-slate-700 py-2 px-4 text-white'
-          onClick={() => setShowDiceRoller(!showDiceRoller)}
-        >
-          Dice Roller
-        </button>
       </div>
-      {showDiceRoller && <DiceRoller />}
       <Yahtzee />
     </main>
   );
