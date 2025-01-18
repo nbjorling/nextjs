8b37b06577aa91d891d547a76f4e7ed7c4447d0b
Niklas Björling <niklas.bjorling@deltaceros.com>
2025-01-18 17:37:22 +0200

    Create client and server api

---
+++ client/pages/mongo.tsx
@@ -0,0 +1,11 @@
+export default function Mongo() {
+  return (
+    <main className='absolute inset-0 z-10 h-screen min-h-screen w-full bg-slate-800 '>
+      <div className='flex h-full w-full items-center justify-center'>
+        <div className='h-1/2 w-1/2 rounded-lg bg-slate-600 p-12'>
+          <div>Test</div>
+        </div>
+      </div>
+    </main>
+  );
+}
---
+++ server/index.js
@@ -0,0 +1,52 @@
+import 'dotenv/config';
+import express from 'express';
+import mongoose from 'mongoose';
+import User from './models/user.js';
+
+const MONGO_NAME = process.env.MONGO_NAME;
+console.log('Koca: MONGO_NAME ', MONGO_NAME);
+const MONGO_PASSWORD = process.env.MONGO_PASSWORD;
+console.log('Koca: MONGO_PASSWORD ', MONGO_PASSWORD);
+const SERVER_PORT = 3000;
+
+const dbUri = `mongodb+srv://${MONGO_NAME}:${MONGO_PASSWORD}@cluster0.jdnoq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+mongoose
+  .connect(dbUri)
+  .then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(SERVER_PORT, () => {
+      console.log(`Server is listening on port ${SERVER_PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log('Failed to connect to MongoDB', err);
+  });
+
+// init app & middleware
+const app = express();
+
+app.get('/users', async (req, res) => {
+  User.find().then((result) => {
+    res.send(result);
+  });
+});
+
+app.get('/add-user', async (req, res) => {
+  const user = new User({
+    email: 'niklas@test.com',
+    firstName: 'John',
+    lastName: 'Doe',
+    password: 'password123',
+    age: 30,
+  });
+
+  user
+    .save()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
